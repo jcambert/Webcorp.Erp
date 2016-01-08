f99c7d8955a0df1780ab554c6ee2c194299cda16
Ambert Jean-Christophe <jc.ambert@free.fr>
2016-01-08 16:54:49 +0100

    Introduce Metro Integration

--- Webcorp.OneDCut/Controllers/HomeController.cs
+++ Webcorp.OneDCut/Controllers/HomeController.cs
@@ -3,15 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webcorp.OneDCut.Models;
 
 namespace Webcorp.OneDCut.Controllers
 {
     public class HomeController : Controller
     {
+        static CutModel cutModel;
+        static HomeController()
+        {
+            cutModel = new CutModel();
+            CreateTempStock(cutModel);
+            CreateTempToCut(cutModel);
+        }
+        public HomeController()
+        {
+
+        }
         public ActionResult Index()
         {
+
             ViewBag.Title = "OneD Cut";
-            return View();
+
+            return View(cutModel);
         }
+
+
+        public ActionResult StockDetail(int index)
+        {
+            return View("StockDetail", cutModel.Stocks.Where(x => x.Id == index).FirstOrDefault());
+        }
+
+        public ActionResult ToCutDetail(int index)
+        {
+            return View("StockDetail", cutModel.ToCut.Where(x => x.Id == index).FirstOrDefault());
+        }
+
+
+        public ActionResult StockCreate()
+        {
+            return View("StockCreate", new Stock());
+        }
+
+        public ActionResult ToCutCreate()
+        {
+            return View("StockCreate", new Stock());
+        }
+
+        [HttpPost]
+        public ActionResult StockCreate(Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["message"] = string.Format("{0} creer", stock.ToString());
+                cutModel.Stocks.Add(stock);
+            }
+            else
+                TempData["erreur"] = string.Format("erreur lors de la creation de {0}", stock.ToString());
+            return View("Index", cutModel);
+        }
+
+        [HttpPost]
+        public ActionResult ToCutCreate(Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["message"] = string.Format("{0} creer", stock.ToString());
+                cutModel.ToCut.Add(stock);
+            }
+            else
+                TempData["erreur"] = string.Format("erreur lors de la creation de {0}", stock.ToString());
+            return View("Index", cutModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult StockDelete(int id)
+        {
+            cutModel.Stocks.Remove(cutModel.Stocks.Where(x => x.Id == id).FirstOrDefault());
+            
+                TempData["message"] = "Suppresion Ok";
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult ToCutDelete(int id)
+        {
+            cutModel.ToCut.Remove(cutModel.ToCut.Where(x => x.Id == id).FirstOrDefault());
+
+            TempData["message"] = "Suppresion Ok";
+
+            return RedirectToAction("Index");
+        }
+
+        private static void CreateTempStock(CutModel cutModel)
+        {
+            var result = new List<Stock>();
+
+            result.Add(new Stock() { Id = 1, Length = 6000, Quantity = 10 });
+            cutModel.Stocks = result;
+        }
+
+        private static void CreateTempToCut(CutModel cutModel)
+        {
+            var result = new List<Stock>();
+            result.Add(new Stock() { Id = 1, Length = 4024, Quantity = 2 });
+            result.Add(new Stock() { Id = 2, Length = 1048, Quantity = 5 });
+            cutModel.ToCut = result;
+        }
+
+
     }
 }
---
+++ Webcorp.OneDCut/Models/CutModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Webcorp.OneDCut.Models
+{
+    public class CutModel
+    {
+        public List<Stock> Stocks { get; set; } = new List<Stock>();
+
+        public List<Stock> ToCut { get; set; } = new List<Stock>();
+
+    }
+
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Longueur")]
+        public int Length { get; set; }
+
+        [Display(Name = "Quantité")]
+        public int Quantity { get; set; }
+    }
+}
